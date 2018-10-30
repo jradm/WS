7b1474ce4c78d1c4dcc4e262fcb814aa5463ce34
JR <janradmacher@gmail.com>
2018-10-30 17:27:55 +0100

    Basic WS functions

---
+++ WS/WebService.asmx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WS
+{
+    /// <summary>
+    /// Zusammenfassungsbeschreibung für WebService
+    /// </summary>
+    [WebService(Namespace = "http://jr.de/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Wenn der Aufruf dieses Webdiensts aus einem Skript zulässig sein soll, heben Sie mithilfe von ASP.NET AJAX die Kommentarmarkierung für die folgende Zeile auf. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public int Add(int a, int b)
+        {
+            return (a + b);
+        }
+
+        [WebMethod]
+        public System.Single Subtract(System.Single A, System.Single B)
+        {
+            return (A - B);
+        }
+
+        [WebMethod]
+        public System.Single Multiply(System.Single A, System.Single B)
+        {
+            return A * B;
+        }
+
+        [WebMethod]
+        public System.Single Divide(System.Single A, System.Single B)
+        {
+            if (B == 0)
+                return -1;
+            return Convert.ToSingle(A / B);
+        }
+    }
+}
